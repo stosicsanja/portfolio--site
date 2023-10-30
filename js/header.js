697733c3ad4778285067b077e7192688c2d40585
stosicsanja <dobrevolje1@gmail.com>
2023-10-30 12:23:52 -0700

    update

--- js/header.js
+++ js/header.js
@@ -12,20 +12,16 @@ if (navToggle) {
 
 /===== MENU HIDDEN =====/;
 
-/* Validate if constant exists */
-
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
 
-/*emOVE MENU MOBILE */
 const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we emove the show-ment navMenu.classList.emove('show-menu')
 
   navMenu.classList.remove("show-menu");
 };
